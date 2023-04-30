3d665a620785201d4be9d31502c35d170be4999c
akhildornala <dornala.akhil16july@gmail.com>
2023-04-30 18:52:59 +0530

    added space

--- src/main/java/com/test/TestClass1.java
+++ src/main/java/com/test/TestClass1.java
@@ -3,6 +3,6 @@ package com.test;
 public class TestClass1 {
 
     public static void main(String args[]){
-        System.out.println("welcome to learnjava8");
+        System.out.println("welcome to learnjava8 ");
     }
 }
