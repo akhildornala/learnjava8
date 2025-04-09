d77c1b8b5b391999a867afe8194fba517f7ecdeb
akhildornala <dornala.akhil16july@gmail.com>
2025-04-09 15:13:39 -0400

    java string reverse

---
+++ src/main/java/com/example/test/ReverseStringExample.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+public class ReverseStringExample {
+
+    public static void main(String args[]) {
+        String input = "madam";
+
+        StringBuilder builder = new StringBuilder(input);
+        String reverse = builder.reverse().toString();
+
+        System.out.println(reverse.equals(input));
+    }
+}
