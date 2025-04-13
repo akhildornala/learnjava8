cc791a219d37d5f7b968fddbcc1b259d256a25a7
akhildornala <dornala.akhil16july@gmail.com>
2025-04-13 18:33:09 -0400

    method overload

---
+++ src/main/java/com/example/corejava/MethodOverloadExample.java
@@ -0,0 +1,20 @@
+package com.example.corejava;
+
+public class MethodOverloadExample {
+
+    public static void main(String args[]){
+        solution(null);
+    }
+
+    private static void solution(Integer a){
+        System.out.println("inside integer method");
+    }
+
+    private static void solution(String a){
+        System.out.println("from string method");
+    }
+
+    private static void solution(Object obj){
+        System.out.println("inside obj method");
+    }
+}
