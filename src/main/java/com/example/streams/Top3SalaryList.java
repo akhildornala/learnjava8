87802ee750cd56396f80ccce2137cca21e911bb3
akhildornala <dornala.akhil16july@gmail.com>
2025-04-16 18:59:26 -0400

    top3 sal

--- src/main/java/com/example/streams/Top3SalaryList.java
+++ src/main/java/com/example/streams/Top3SalaryList.java
@@ -3,6 +3,7 @@ package com.example.streams;
 import com.example.test.Employee;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -18,9 +19,20 @@ public class Top3SalaryList {
                 .collect(Collectors.toList());
         System.out.println(top3SalList);
 
-        Map<String, Employee> employeeMap= employeeList.stream()
-                .collect(Collectors.toMap(Employee::geteName, Function.identity(), (oldVal, newVal)->newVal));
+        Map<String, Employee> employeeMap = employeeList.stream()
+                .collect(Collectors.toMap(Employee::geteName, Function.identity(), (oldVal, newVal) -> newVal));
         System.out.println(employeeMap);
+
+        /*
+        Map<String,List<Dish>> dishMap = menu.stream().collect(Collectors.groupingBy(
+            Dish::getType,
+            LinkedHashMap::new, Collectors.toList()));
+         */
+
+
+        Map<String, List<Employee>> employeeMap2 = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::geteName, LinkedHashMap::new, Collectors.toList()));
+        System.out.println(employeeMap2);
 
 
     }
