3d946cef2005a828e52262c06f8c3832bad1c153
akhildornala <dornala.akhil16july@gmail.com>
2025-04-18 18:10:20 -0400

    treemap

---
+++ src/main/java/com/example/test/TreeMapExample.java
@@ -0,0 +1,58 @@
+package com.example.test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TreeMapExample {
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = Employee.getEmployeeDefaultData();
+
+
+        Map<String, Employee> treeMap = new TreeMap<>();
+        treeMap = employeeList.stream()
+                .collect(Collectors.toMap(Employee::geteName, v->v, (v1,v2)->v1, TreeMap::new));
+        employeeList.stream()
+                        .collect(Collectors.groupingBy(Employee::geteName,TreeMap::new, Collectors.toList()));
+        treeMap.entrySet()
+                .stream()
+                .forEach(System.out::println);
+        System.out.println("#######################################");
+        for (Map.Entry<String, Employee> entry : treeMap.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+
+        List<String> names = List.of("Alice", "Bob", "Charlie", "David");
+
+        // Convert the list to a TreeMap where keys are names and values are also names
+        TreeMap<String, String> nameMap = convertListToTreeMap(names, name -> name);
+        System.out.println(nameMap);
+
+        List<Integer> numbers = List.of(5, 2, 8, 1, 9);
+
+        // Convert the list to a TreeMap where keys are numbers and values are their squares
+        TreeMap<Integer, Integer> numberMap = convertListToTreeMap(numbers, number -> number);
+        System.out.println(numberMap);
+
+        TreeMap<String, Employee> employeeTreeMap = convertListToTreeMap(employeeList, Employee::geteName);
+        System.out.println(employeeTreeMap);
+
+        Map<String, Employee> customTreeMap = generateTreeMap(employeeList);
+        System.out.println("custom");
+        System.out.println(customTreeMap);
+
+
+    }
+
+    public static <K, V> TreeMap<K, V> convertListToTreeMap(List<V> list, java.util.function.Function<V, K> keyMapper) {
+        return list.stream().collect(Collectors.toMap(keyMapper, v -> v, (v1, v2) -> v1, TreeMap::new));
+    }
+
+    public static TreeMap<String,Employee> generateTreeMap(List<Employee> employeeList) {
+        TreeMap<String,Employee> treeMap =  employeeList.stream().collect(Collectors.toMap(Employee::geteName, v->v, (v1,v2)->v1, TreeMap::new));
+        return treeMap;
+    }
+}
