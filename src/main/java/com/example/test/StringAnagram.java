73e741e25e5469a6349e33275e5e28f29026f6d1
akhildornala <dornala.akhil16july@gmail.com>
2025-05-07 17:48:01 -0400

    string anagram

---
+++ src/main/java/com/example/test/StringAnagram.java
@@ -0,0 +1,38 @@
+package com.example.test;
+
+public class StringAnagram {
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    private static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[256]; // Assuming ASCII
+
+        for (int i = 0; i < str1.length(); i++) {
+//            System.out.println(str1.charAt(i));
+            System.out.println(charCount[str1.charAt(i)]);
+            charCount[str1.charAt(i)]++;
+            charCount[str2.charAt(i)]--;
+        }
+
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
