1fc18a1f69bc19fa7080e3975717afad4601a8d2
akhildornala <dornala.akhil16july@gmail.com>
2025-05-08 17:19:43 -0400

    dummy commit

--- src/main/java/com/example/test/CallStaticWithNullReference.java
+++ src/main/java/com/example/test/CallStaticWithNullReference.java
@@ -11,5 +11,6 @@ public class CallStaticWithNullReference {
             System.out.println(emp.getEmployeeDefaultData());
 
             Collections.synchronizedMap(new HashMap<>());
+
         }
 }
