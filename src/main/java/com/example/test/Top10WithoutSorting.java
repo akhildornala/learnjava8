4fd70200b207c3b16d279a80d1005f3e63215a57
akhildornala <dornala.akhil16july@gmail.com>
2025-05-14 22:44:20 -0700

    dummy commit

---
+++ src/main/java/com/example/test/Top10WithoutSorting.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Top10WithoutSorting {
+
+    public static void main(String args[]){
+
+        List<Integer> ls = new ArrayList<>(List.of(99, 4, 1, 13, 56, 7, 14, 8,  154, 256, 72));
+        List<Integer> ls2 = new ArrayList<>(List.of(99, 4, 1, 13, 56, 7, 14, 8,  154, 256, 72));
+        Collections.sort(ls2);
+        Collections.reverse(ls2);
+        ls2.stream()
+                .limit(5)
+                .forEach(System.out::println);
+
+        int[] arr = new int[10];
+        arr[0] = ls.get(0);
+
+        for(int i=0; i< ls.size(); i++){
+
+        }
+
+
+
+    }
+}
